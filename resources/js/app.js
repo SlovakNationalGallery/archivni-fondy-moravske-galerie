62c2eeb1f789ffb438d7493be44f103dfada05d1
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2021-12-09 02:41:08 +0100

    setup vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,20 @@
+import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import App from './App.vue'
+import Catalog from './views/Catalog.vue'
+
+
 require('./bootstrap');
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [{
+        path: '/',
+        component: Catalog,
+    }]
+})
+
+const app = createApp(App)
+    .use(router)
+
+app.mount('#app')
---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width,initial-scale=1.0">
+        <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+        <title>Archivní fondy Moravské galerie</title>
+    </head>
+    <body class="antialias bg-black text-base text-white">
+        <noscript>
+            <strong>We're sorry but this site doesn't work properly without JavaScript enabled. Please enable it to continue.</strong>
+        </noscript>
+        <div id="app"></div>
+        <script src="{{ mix('/js/app.js') }}"></script>
+    </body>
+</html>
