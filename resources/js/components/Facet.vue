b9c5c67cf120e5d51e408cf9b9551c5fa8bfb91b
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2022-01-05 19:12:24 +0100

    multiselect

--- resources/js/components/Facet.vue
+++ resources/js/components/Facet.vue
@@ -1,12 +1,29 @@
 <template>
-    <select class="block p-2 w-full" v-model="value" @update:modelValue="$emit('update', value)">
-        <option :value="null">{{ label }}</option>
-        <option v-for="(count, key) in options" :key="key" :value="key">{{ key }} [{{ count }}]</option>
-    </select>
+    <Multiselect
+    v-model="value"
+    :placeholder="label"
+    :options="options"
+    :searchable="true"
+    @change="(value) => { $emit('update', value) }"
+    >
+    <template v-slot:option="{ option }">
+        {{ option.value }} [{{ option.label }}]
+    </template>
+    <template v-slot:singlelabel="{ value }">
+        <div class="multiselect-single-label">
+            {{ value.value }}
+        </div>
+    </template>
+    </Multiselect>
 </template>
 
+<style src="@vueform/multiselect/themes/default.css"></style>
+
 <script>
+import Multiselect from '@vueform/multiselect'
+
 export default {
+    components: { Multiselect },
     props: ['value', 'options', 'label'],
     emits: ['update'],
 }
